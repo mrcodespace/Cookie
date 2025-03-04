4f4c17c64f6c50801e2616d403d5d952a335ab04
DarksoulLP <jonasweidemann200586@gmail.com>
2025-03-04 12:00:31 +0100

    1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbCookie = new System.Windows.Forms.PictureBox();
+            this.lblCookies = new System.Windows.Forms.Label();
+            this.btnBuyAutoClicker = new System.Windows.Forms.Button();
+            this.btnBuyMultiplier = new System.Windows.Forms.Button();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCookie)).BeginInit();
+            this.SuspendLayout();
+
+            // pbCookie
+            this.pbCookie.Image = global::WindowsFormsApp1.Properties.Resources.cookie;
+            this.pbCookie.Location = new System.Drawing.Point(75, 50);
+            this.pbCookie.Name = "pbCookie";
+            this.pbCookie.Size = new System.Drawing.Size(150, 150);
+            this.pbCookie.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCookie.TabIndex = 0;
+            this.pbCookie.TabStop = false;
+            this.pbCookie.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PbCookie_MouseDown);
+
+            // lblCookies
+            this.lblCookies.AutoSize = true;
+            this.lblCookies.Font = new System.Drawing.Font("Arial", 14F);
+            this.lblCookies.Location = new System.Drawing.Point(50, 200);
+            this.lblCookies.Name = "lblCookies";
+            this.lblCookies.Size = new System.Drawing.Size(200, 30);
+            this.lblCookies.TabIndex = 1;
+            this.lblCookies.Text = "0 Cookies";
+
+            // btnBuyAutoClicker
+            this.btnBuyAutoClicker.Location = new System.Drawing.Point(50, 250);
+            this.btnBuyAutoClicker.Name = "btnBuyAutoClicker";
+            this.btnBuyAutoClicker.Size = new System.Drawing.Size(200, 30);
+            this.btnBuyAutoClicker.TabIndex = 2;
+            this.btnBuyAutoClicker.Text = "Autoklicker kaufen (15)";
+            this.btnBuyAutoClicker.UseVisualStyleBackColor = true;
+            this.btnBuyAutoClicker.Click += new System.EventHandler(this.BtnBuyAutoClicker_Click);
+
+            // btnBuyMultiplier
+            this.btnBuyMultiplier.Location = new System.Drawing.Point(50, 300);
+            this.btnBuyMultiplier.Name = "btnBuyMultiplier";
+            this.btnBuyMultiplier.Size = new System.Drawing.Size(200, 30);
+            this.btnBuyMultiplier.TabIndex = 3;
+            this.btnBuyMultiplier.Text = "Multiplier x2 (100)";
+            this.btnBuyMultiplier.UseVisualStyleBackColor = true;
+            this.btnBuyMultiplier.Click += new System.EventHandler(this.BtnBuyMultiplier_Click);
+
+            // gameTimer
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 1000;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+
+            // MainForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 400);
+            this.Controls.Add(this.btnBuyMultiplier);
+            this.Controls.Add(this.btnBuyAutoClicker);
+            this.Controls.Add(this.lblCookies);
+            this.Controls.Add(this.pbCookie);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.Text = "Cookie Clicker";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCookie)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCookie;
+        private System.Windows.Forms.Label lblCookies;
+        private System.Windows.Forms.Button btnBuyAutoClicker;
+        private System.Windows.Forms.Button btnBuyMultiplier;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        private int cookies = 0;
+        private int clickPower = 1;
+        private int autoClickerCost = 15;
+        private int autoClickers = 0;
+        private int multiplierCost = 100;
+        private int multiplierCount = 1;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadGame();
+            gameTimer.Start();
+        }
+
+        // Initialisierung der Komponenten
+        private void InitializeComponent()
+        {
+            // UI-Elemente
+            pbCookie = new PictureBox();
+            lblCookies = new Label();
+            btnBuyAutoClicker = new Button();
+            btnBuyMultiplier = new Button();
+            gameTimer = new Timer();
+
+            // PictureBox (Cookie)
+            pbCookie.Image = WindowsFormsApp1.Properties.Resources.cookie;
+            // Fügen Sie ein Cookie-Bild hinzu
+            pbCookie.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbCookie.Size = new Size(150, 150);
+            pbCookie.Location = new Point(75, 50);
+            pbCookie.MouseDown += PbCookie_MouseDown;
+
+            // Labels
+            lblCookies.Font = new Font("Arial", 14F);
+            lblCookies.Location = new Point(50, 200);
+            lblCookies.Size = new Size(200, 30);
+
+            // Buttons
+            btnBuyAutoClicker.Text = $"Autoklicker kaufen ({autoClickerCost})";
+            btnBuyAutoClicker.Location = new Point(50, 250);
+            btnBuyAutoClicker.Click += BtnBuyAutoClicker_Click;
+
+            btnBuyMultiplier.Text = $"Multiplier x2 ({multiplierCost})";
+            btnBuyMultiplier.Location = new Point(50, 300);
+            btnBuyMultiplier.Click += BtnBuyMultiplier_Click;
+
+            // Timer
+            gameTimer.Interval = 1000;
+
+            // Form
+            ClientSize = new Size(300, 400);
+            Controls.AddRange(new Control[] { pbCookie, lblCookies, btnBuyAutoClicker, btnBuyMultiplier });
+            Text = "Cookie Clicker";
+            DoubleBuffered = true;
+        }
+
+        private void PbCookie_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                cookies += clickPower * multiplierCount;
+                UpdateUI();
+                CreateClickEffect();
+            }
+        }
+
+        private void BtnBuyAutoClicker_Click(object sender, EventArgs e)
+        {
+            if (cookies >= autoClickerCost)
+            {
+                cookies -= autoClickerCost;
+                autoClickers++;
+                autoClickerCost = (int)(autoClickerCost * 1.15);
+                UpdateUI();
+            }
+        }
+
+        private void BtnBuyMultiplier_Click(object sender, EventArgs e)
+        {
+            if (cookies >= multiplierCost)
+            {
+                cookies -= multiplierCost;
+                multiplierCount *= 2;
+                multiplierCost *= 4;
+                UpdateUI();
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            cookies += autoClickers * multiplierCount;
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(UpdateUI));
+                return;
+            }
+
+            lblCookies.Text = $"{cookies} Cookies";
+            btnBuyAutoClicker.Text = $"Autoklicker ({autoClickerCost})";
+            btnBuyMultiplier.Text = $"Multiplier x{multiplierCount * 2} ({multiplierCost})";
+            btnBuyAutoClicker.Enabled = cookies >= autoClickerCost;
+            btnBuyMultiplier.Enabled = cookies >= multiplierCost;
+        }
+
+        private void CreateClickEffect()
+        {
+            var effect = new Label
+            {
+                Text = $"+{clickPower * multiplierCount}",
+                Location = new Point(pbCookie.Right + 10, pbCookie.Top + 50),
+                ForeColor = Color.Green,
+                BackColor = Color.Transparent,
+                AutoSize = true
+            };
+
+            Controls.Add(effect);
+            effect.BringToFront();
+
+            var timer = new Timer { Interval = 20 };
+            int y = effect.Top;
+            timer.Tick += (s, args) =>
+            {
+                y -= 2;
+                effect.Top = y;
+                effect.ForeColor = Color.FromArgb(effect.ForeColor.A - 2, effect.ForeColor);
+
+                if (effect.ForeColor.A <= 2)
+                {
+                    Controls.Remove(effect);
+                    timer.Stop();
+                }
+            };
+            timer.Start();
+        }
+
+        private void LoadGame()
+        {
+            cookies = WindowsFormsApp1.Properties.Settings.Default.cookie;
+            autoClickers = WindowsFormsApp1.Properties.Settings.Default.AutoClickers;
+            multiplierCount = WindowsFormsApp1.Properties.Settings.Default.Multiplier;
+            UpdateUI();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            WindowsFormsApp1.Properties.Settings.Default.cookie = cookies;
+            WindowsFormsApp1.Properties.Settings.Default.AutoClickers = autoClickers;
+            WindowsFormsApp1.Properties.Settings.Default.Multiplier = multiplierCount;
+            WindowsFormsApp1.Properties.Settings.Default.Save();
+        }
+
+        // UI Elemente
+        private PictureBox pbCookie;
+        private Label lblCookies;
+        private Button btnBuyAutoClicker;
+        private Button btnBuyMultiplier;
+        private Timer gameTimer;
+    }
+}
